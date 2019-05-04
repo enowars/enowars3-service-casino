0a3e48510c3ac1fd0aba051266694f3343057319
David <david.p.runge@campus.tu-berlin.de>
2019-05-04 11:29:58 +0200

    Output: JSON, encoded; iv obfuscated as cryptomaterial

--- casino/cryptomat/sender.jl
+++ casino/cryptomat/sender.jl
@@ -1,5 +1,6 @@
 using AES
 using MD5
+using JSON
 
 function padMessage(message)
     cur_nr_of_blocks = length(message)÷16
@@ -8,7 +9,7 @@ function padMessage(message)
     return vcat(message, fill_zeros)
 end
 
-function encryptMessage(mode::Int, message::String, key, iv)
+function encryptMessage(mode::Int, message::String, cryptomaterial::Array{Array{UInt8,1},1})
     message = Array{UInt8, 1}(message)
     #println("Prepad message: ", message)
     #println("Prepad message length: ", length(message))
@@ -17,29 +18,30 @@ function encryptMessage(mode::Int, message::String, key, iv)
     #println("Postpad message length: ", length(message))
     if mode == 1
         println("AES CBC:")
-        AESCBC(message, key, iv, true)
+        AESCBC(message, cryptomaterial[1], cryptomaterial[2], true)
     elseif mode == 2
         println("AES CFB:")
-        AESCFB(message, key, iv, true)
+        AESCFB(message, cryptomaterial[1], cryptomaterial[2], true)
     elseif mode == 3
         println("AES CTR:")
-        AESCTR(message, key, iv)
+        AESCTR(message, cryptomaterial[1], cryptomaterial[2])
     elseif mode == 4
         println("AES ECB:")
-        AESECB(message, key, true)
+        AESECB(message, cryptomaterial[1], true)
     else
         println("AES OFB:")
-        AESOFB(message, key, iv)
+        AESOFB(message, cryptomaterial[1], cryptomaterial[2])
     end
 
 end
 
 function generate_cryptomaterial()
-    key = md5("This secret is deliviered by the flagbot")
-    iv = rand(UInt8, 16)
-    key = vcat(key, md5(iv))
-
-    return key, iv
+    #TODO: insert symmetric key here
+    cryptomaterial = Array{Array{UInt8,1}, 1}(undef, 2)
+    key = md5("This is the symmetric key. Need to be computed once for each team.")
+    cryptomaterial[2] = rand(UInt8, 16)
+    cryptomaterial[1] = vcat(key, md5(cryptomaterial[2]))
+    return cryptomaterial
 end
 
 
@@ -47,19 +49,20 @@ end
 function sendSecret(mode::Int)
     println("I will now send you the message in the choosen mode.")
 
+    #insert flag here
     messages = ["ATOM-BOMB-CODE-START",
                 "FLAG",
                 "ATOM-BOMB-CODE-END"]
 
-    key, iv = generate_cryptomaterial()
+    cryptomaterial = generate_cryptomaterial()
 
     for cur_message in messages
         println("\n", cur_message)
-        enc_Msg = encryptMessage(mode, cur_message, key, iv)
+        enc_Msg = encryptMessage(mode, cur_message, cryptomaterial)
         #print iv if needed
+        full_Msg = [enc_Msg, cryptomaterial[2]]
         println("Message:")
-        println(enc_Msg)
-        println(iv)
+        println(JSON.json(full_Msg))
     end
 
     println("Sending finished")
--- pkgInstallation.jl
+++ pkgInstallation.jl
@@ -1,3 +1,8 @@
 using Pkg
 
-pkg"add https://github.com/DopeforHope/AES.jl"
+
+pkg"add https://github.com/faf0/AES.jl"
+pkg"add https://github.com/oxinabox/MD5.jl"
+pkg"add JSON"
+pkg"add LegacyStrings"
+pkg"resolve"

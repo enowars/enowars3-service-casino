0e31f599c55fc2b1d946b417d1fba0cf7699dd94
David <david.p.runge@campus.tu-berlin.de>
2019-05-11 21:47:59 +0200

    now reads aes key from file

--- casino/cryptomat/sender.jl
+++ casino/cryptomat/sender.jl
@@ -38,7 +38,10 @@ end
 function generate_cryptomaterial()
     #TODO: insert symmetric key here
     cryptomaterial = Array{Array{UInt8,1}, 1}(undef, 2)
-    key = md5("This is the symmetric key. Need to be computed once for each team.")
+    f = open("assets/aes.seed", "r")
+    aesSeed = read(f)
+    close(f)
+    key = md5(aesSeed)
     cryptomaterial[2] = rand(UInt8, 16)
     cryptomaterial[1] = vcat(key, md5(cryptomaterial[2]))
     return cryptomaterial

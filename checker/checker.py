e07f951f4c0dca4e9ad508013c8db198b1fc2f94
Hanno Wolf Skowronek <h.skowronek@campus.tu-berlin.de>
2019-05-19 18:21:32 +0200

    first start on the checker putflag..

--- checker/checker.py
+++ checker/checker.py
@@ -3,19 +3,41 @@ import json
 import random
 import string
 from enochecker import BaseChecker, BrokenServiceException, run
+from enochecker.utils import *
 
 session = requests.Session()
 
-
 class CasinoChecker(BaseChecker):
+
+    with open('../service/casino/data/strings.json', 'r') as f:
+        dictionary = json.load(f)
 
     port = 6969  # default port to send requests to.
 
     def putflag(self):
         t = self.connect()
         self.debug("connected to {}".format(self.address))
-        
-        pass
+        readline_expect(t, "Entering...")
+        readline_expect(t, self.dictionary["welcome"], read_until = self.dictionary["welcome"][-10:])
+        if self.flag_idx == 0:
+            #table-flag
+
+            #change to games
+            t.write("g")
+            #readline_expect(t, self.dictionary["withdraw_0"], read_until = self.dictionary["withdraw_0"][-10])
+            
+            #change to black_jack
+            t.write("black_jack")
+
+            #create a new table
+            t.write("c")
+            t.write("1337")
+            t.write("ENOawecopaiwjencao2983ncaownecaow=")
+            t.write("1000000000")
+            t.write("password")
+        elif self.flag_idx == 1:
+            #cryptomat-flag
+            pass
 
     def getflag(self):
         pass
@@ -32,4 +54,4 @@ class CasinoChecker(BaseChecker):
 
 app = CasinoChecker.service
 if __name__ == "__main__":
-    run(CasninoChecker)
+    run(CasinoChecker)
